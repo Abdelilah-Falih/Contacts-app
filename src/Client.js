c4d996ae543eb91f8ea73d9e381235749002acfd
Abdelilah-Falih <121343981+Abdelilah-Falih@users.noreply.github.com>
2023-02-04 12:59:22 +0100

    first commit

--- src/Client.js
+++ src/Client.js
@@ -1,8 +1,5 @@
-
-
 import  {ApolloClient}  from "@apollo/client";
 import { InMemoryCache } from "apollo-cache-inmemory";
-import { WebSocketLink } from "apollo-link-ws";
 
 const client = new ApolloClient({
     uri:"https://contact-app-react.hasura.app/v1/graphql",
@@ -13,18 +10,4 @@ const client = new ApolloClient({
 
 })
 
-/*const client2 = new ApolloClient({
-    link:new WebSocketLink({
-        uri:'ws://contact-app-react.hasura.app/v1/graphql',
-        options:{
-            reconnect:true
-        }
-    }),
-    cache:new InMemoryCache(),
-    headers: {
-        'x-hasura-admin-secret': 'cAyLKOD5znhWwYtwTOAAbtp2pNB1mxYsj9tUVCgIjFGnirweDfnJKuhx7ssngQMF',
-    },
-
-
-})*/
 export default client;
